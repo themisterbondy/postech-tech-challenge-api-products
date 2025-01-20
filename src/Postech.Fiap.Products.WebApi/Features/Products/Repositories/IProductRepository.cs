69ebfc2c0f6dd2ed4ff7628328fa09044a6b20e4
Alexandre Nascimento <alexandre.n@outlook.com>
2025-01-19 21:05:49 -0300

    Add `IProductRepository` interface and `ProductRepository` class
    This change introduces a repository pattern for product management. The `IProductRepository` defines CRUD operations, while `ProductRepository` implements them using MongoDB. This provides a structured approach to handling product data storage and retrieval.

---
+++ src/Postech.Fiap.Products.WebApi/Features/Products/Repositories/IProductRepository.cs
@@ -0,0 +1,12 @@
+using Postech.Fiap.Products.WebApi.Features.Products.Entities;
+
+namespace PosTech.MyFood.WebApi.Features.Products.Repositories;
+
+public interface IProductRepository
+{
+    Task<Product?> FindByIdAsync(Guid id, CancellationToken cancellationToken);
+    Task<List<Product?>> FindByCategoryAsync(ProductCategory? category, CancellationToken cancellationToken);
+    Task<Product?> CreateAsync(Product? product, CancellationToken cancellationToken);
+    Task<Product?> UpdateAsync(Product? product, CancellationToken cancellationToken);
+    Task DeleteAsync(Product? product, CancellationToken cancellationToken);
+}
